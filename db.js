b8584f7b2d33e2edc9985ea3961486b522330316
Mirtles <mirte.post@yahoo.co.uk>
2019-09-04 12:00:06 +0200

    Add heroku db log.

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:himitsu@loc
 const db = new Sequelize(databaseUrl)
 
 db.sync()
-  .then(() => console.log("updated database"))
+  .then(() => console.log(`updated database (running on ${databaseUrl}`))
   .catch(console.error)
 
 module.exports = db
